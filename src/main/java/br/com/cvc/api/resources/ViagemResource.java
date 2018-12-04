78b36c88e1b5c1c3c2e86fff2e5262ff44734ae0
Andre Luis Conceicao Da Fonseca <acfonseca@brq.com>
2018-12-04 11:40:48 -0200

    alteracoes

--- src/main/java/br/com/cvc/api/resources/ViagemResource.java
+++ src/main/java/br/com/cvc/api/resources/ViagemResource.java
@@ -3,6 +3,7 @@ package br.com.cvc.api.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,19 @@ public class ViagemResource {
 
 	@Autowired
 	private HotelService hotelService;
-	
-	
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getIt() {
-		
+	public String getIt(@QueryParam("CityCode") int cityCode, @QueryParam("Checkin") String checkin,
+			@QueryParam("Checkout") String checkout, @QueryParam("qtdAdult") int qtdAdult,
+			@QueryParam("qtdChild") int qtdChild) {
+
 		System.out.println(hotelService);
+		System.out.println("cityCode:"+cityCode);
+		System.out.println("checkin:"+checkin);
+		System.out.println("checkout:"+checkout);
+		System.out.println("qtdAdult:"+qtdAdult);
+		System.out.println("qtdChild:"+qtdChild);
 		return "Got it!";
 	}
 }
